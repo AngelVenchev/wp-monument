426ad864c53f72a593c7b4471e1219cb7d2d31b1
angelvenchev <angelvenchev@yahoo.com>
2017-08-02 00:37:53 +0300

    Remove link to prev and next post

--- wp-content/themes/saka/single.php
+++ wp-content/themes/saka/single.php
@@ -18,15 +18,7 @@ get_header(); ?>
 
 				get_template_part( 'template-parts/content', get_post_format() );
 
-				the_post_navigation( array (
-					'prev_text' => '<span class="fa fa-chevron-circle-left" aria-hidden="true"></span><span class="screen-reader-text">' . __( 'Previous Post', 'saka' ) . '</span>%title',
-					'next_text' => '<span class="screen-reader-text">' . __( 'Next Post', 'saka' ) . '</span>%title<span class="fa fa-chevron-circle-right" aria-hidden="true"></span>'
-				) );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+				
 
 			endwhile; // End of the loop.
 			?>
