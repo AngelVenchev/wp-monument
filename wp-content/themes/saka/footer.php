0bbda880aa55a63e03b33e6b086939007c78c33b
angelvenchev <angelvenchev@yahoo.com>
2017-08-02 00:36:29 +0300

    Remove footer

--- wp-content/themes/saka/footer.php
+++ wp-content/themes/saka/footer.php
@@ -13,28 +13,15 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="container footer-inner">
-			<div class="site-info">
-			<?php
-				$description = get_bloginfo( 'description', 'display' );
-				if ( $description || is_customize_preview() ) : ?>
-					<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-				<?php
-				endif; ?>
-
-				<span class="copyright">&copy; <?php bloginfo( 'name' ); ?></span>
-				<span class="sep"> | </span>
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'saka' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'saka' ), 'WordPress' ); ?></a>
-			</div><!-- .site-info -->
-		</div><!-- .container / footer-inner -->
-	</footer><!-- #colophon -->
+	
 
 	<div id="drawer-mask"></div><!-- #drawer-mask -->
 
 </div><!-- #page -->
 
+
 <?php wp_footer(); ?>
+
 
 </body>
 </html>
